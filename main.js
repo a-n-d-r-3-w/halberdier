b685152e693c4402cb9d556ead12b753718fb056
616e64726577 <616e64726577+github@protonmail.com>
2018-02-09 00:10:45 -0500

    Cleanup

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
-const electron = require('electron')
-const {ipcMain} = require('electron')
+const electron = require('electron');
+const {ipcMain} = require('electron');
 // Module to control application life.
-const app = electron.app
+const app = electron.app;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
 
-const path = require('path')
-const url = require('url')
+const path = require('path');
+const url = require('url');
 
 const fs = require('fs');
 const crypto = require('crypto');
 
 const os = require('os');
 
-// const passwordsFilePath = path.join(__dirname, "passwords.json");
 const passwordsFilePath = os.homedir() + "/passwords.json";
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function encrypt(string, savePassword) {
     const cipher = crypto.createCipher('aes192', savePassword);
@@ -36,17 +35,17 @@ function decrypt(string, loadPassword) {
 
 function createWindow() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 700})
+    mainWindow = new BrowserWindow({width: 800, height: 700});
 
     // and load the index.html of the app.
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true
-    }))
+    }));
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools();
 
     ipcMain.on('get-passwords', (event, loadPassword) => {
         const fromFile = fs.readFileSync(passwordsFilePath, 'utf8');
@@ -84,7 +83,7 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -93,7 +92,7 @@ app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
         app.quit()
     }
-})
+});
 
 app.on('activate', function () {
     // On OS X it's common to re-create a window in the app when the
@@ -101,7 +100,7 @@ app.on('activate', function () {
     if (mainWindow === null) {
         createWindow()
     }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
