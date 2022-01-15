e0153d04ad66d343d49da5f7a19036115526e27a
andr3w <5803467+a-n-d-r-3-w@users.noreply.github.com>
2022-01-15 18:34:21 -0500

    Update HListItem to use the latest version of MUI

--- src/HListItem.jsx
+++ src/HListItem.jsx
@@ -1,29 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { ListItem } from "material-ui/List";
-import Tooltip from "material-ui/Tooltip";
-import Input, { InputLabel, InputAdornment } from "material-ui/Input";
-import { FormControl } from "material-ui/Form";
-import IconButton from "material-ui/IconButton";
-import DeleteIcon from "material-ui-icons/Delete";
-import CopyIcon from "material-ui-icons/ContentCopy";
-import { withStyles } from "material-ui/styles";
+import ListItem from "@mui/material/ListItem";
+import Tooltip from "@mui/material/Tooltip";
+import Input from "@mui/material/Input";
+import InputAdornment from "@mui/material/InputAdornment";
+import InputLabel from "@mui/material/InputLabel";
 
-const styles = (theme) => ({
-  formControl: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
-  },
-  iconButton: {
-    height: theme.typography.fontSize * 2,
-  },
-  icon: {
-    fontSize: theme.typography.fontSize * 1.5,
-  },
-});
+import FormControl from "@mui/material/FormControl";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import CopyIcon from "@mui/icons-material/ContentCopy";
 
 const HListItem = (props) => {
-  const { classes, item } = props;
+  const { item } = props;
 
   return (
     <ListItem key={item.id} dense>
@@ -32,14 +21,14 @@ const HListItem = (props) => {
           <DeleteIcon />
         </IconButton>
       </Tooltip>
-      <FormControl className={classes.formControl}>
+      <FormControl>
         <InputLabel>Name</InputLabel>
         <Input
           value={item.service}
           onChange={props.onChange(item.id, "service")}
         />
       </FormControl>
-      <FormControl className={classes.formControl}>
+      <FormControl>
         <InputLabel>Username</InputLabel>
         <Input
           endAdornment={
@@ -50,14 +39,13 @@ const HListItem = (props) => {
               <Tooltip title="Copy username">
                 <div>
                   <IconButton
-                    className={classes.iconButton}
                     color="primary"
                     disabled={
                       !props.items.find((password) => password.id === item.id)
                         .username
                     }
                   >
-                    <CopyIcon className={classes.icon} />
+                    <CopyIcon />
                   </IconButton>
                 </div>
               </Tooltip>
@@ -67,7 +55,7 @@ const HListItem = (props) => {
           onChange={props.onChange(item.id, "username")}
         />
       </FormControl>
-      <FormControl className={classes.formControl}>
+      <FormControl>
         <InputLabel>Password</InputLabel>
         <Input
           type={props.showPasswords ? "text" : "password"}
@@ -79,14 +67,13 @@ const HListItem = (props) => {
               <Tooltip title="Copy password">
                 <div>
                   <IconButton
-                    className={classes.iconButton}
                     color="primary"
                     disabled={
                       !props.items.find((password) => password.id === item.id)
                         .password
                     }
                   >
-                    <CopyIcon className={classes.icon} />
+                    <CopyIcon />
                   </IconButton>
                 </div>
               </Tooltip>
@@ -101,7 +88,6 @@ const HListItem = (props) => {
 };
 
 HListItem.propTypes = {
-  classes: PropTypes.object,
   items: PropTypes.array.isRequired,
   showPasswords: PropTypes.bool.isRequired,
   item: PropTypes.shape({
@@ -115,4 +101,4 @@ HListItem.propTypes = {
   copyField: PropTypes.func.isRequired,
 };
 
-export default withStyles(styles)(HListItem);
+export default HListItem;
